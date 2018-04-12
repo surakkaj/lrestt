d1fa140fc04c9e95c12e7aaa7395cc2aed3feb50
surakkaj <surakkaj@cs.helsinki.fi>
2018-04-12 16:27:23 +0300

    updated bookphp

--- api/book.php
+++ api/book.php
@@ -2,7 +2,7 @@
         $isbn = $_POST["isbn"];
         $ch = curl_init();
 
-        curl_setopt($ch, CURLOPT_URL, "https://openlibrary.org/api/books?bibkeys=ISBN:"$isbn"&jscmd=data");
+        curl_setopt($ch, CURLOPT_URL, "https://openlibrary.org/api/books?bibkeys=ISBN:$isbn&jscmd=data");
 
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
